adb4f2b5efc718461d4d33640d57c0685a390588
Israel Silva <israel.bernardes.silva@gmail.com>
2023-02-20 20:07:47 -0300

    feat: extract process numbers by diaries

--- core/extract_processes_tst.py
+++ core/extract_processes_tst.py
@@ -1,4 +1,6 @@
+import time
 from datetime import timedelta
+from pathlib import Path
 
 import pendulum
 from selenium import webdriver
@@ -7,7 +9,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from services.services_browser import ServicesBrowser
 from settings import (COURT_ID, END_DATE_ID, INITIAL_DATE_ID,
-                      SEARCH_BUTTON_XPATH, URL, TABLE_DIARIES_ID)
+                      SEARCH_BUTTON_XPATH, TABLE_DIARIES_ID, URL)
 
 
 class ExtractProcessesTST:
@@ -22,20 +24,31 @@ class ExtractProcessesTST:
         browser.get(URL)
         return browser
 
-    def search_last_week_tst_diaries(self) -> None:
+    def search_last_week_tst_diaries(self) -> dict:
         start_date, end_date = self.get_start_end_dates_week()
         self.services_browser.fill_input_field(INITIAL_DATE_ID, start_date)
         self.services_browser.fill_input_field(END_DATE_ID, end_date)
         self.services_browser.fill_select_field_by_index(COURT_ID, 1)
         self.services_browser.click_button(SEARCH_BUTTON_XPATH)
 
-    def _download_last_week_tst_diaries(self) -> None:
+        return self._download_last_week_tst_diaries()
+
+    def _download_last_week_tst_diaries(self) -> dict:
         table_with_diaries = self.browser.find_element(By.ID, TABLE_DIARIES_ID)
         rows = table_with_diaries.find_elements(By.TAG_NAME, "tr")[1:]
         first_line_table = 2
 
+        diaries_separated_by_date = {}
         for index, row in enumerate(rows, start=first_line_table):
+            availability_date = self.browser.find_element(
+                By.XPATH, f'//*[@id="diarioCon"]/fieldset/table/tbody/tr[{index}]/td[1]/span').text
+            title = self.browser.find_element(
+                By.XPATH, f'//*[@id="diarioCon"]/fieldset/table/tbody/tr[{index}]/td[2]/span').text
             self.services_browser.click_button(f'//*[@id="diarioCon"]/fieldset/table/tbody/tr[{index}]/td[3]/button')
+            self.is_download_finished()
+            self.group_diaries_by_date(diaries_separated_by_date, availability_date, title)
+
+        return diaries_separated_by_date
 
     @staticmethod
     def get_start_end_dates_week() -> tuple[str, str]:
@@ -47,3 +60,18 @@ class ExtractProcessesTST:
         end_date = (today.end_of('week') - timedelta(7)).strftime('%d/%m/%Y')
 
         return start_date, end_date
+
+    @staticmethod
+    def is_download_finished() -> None:
+        path_download = '/home/suporte/Downloads'  # TODO: para windows muda
+        while True:
+            if not sorted(Path(path_download).glob('*.crdownload')):
+                break
+            time.sleep(1)
+
+    @staticmethod
+    def group_diaries_by_date(diaries_separated_by_date: dict, availability_date: str, title: str) -> None:
+        if availability_date not in diaries_separated_by_date:
+            diaries_separated_by_date[availability_date] = [title]
+        else:
+            diaries_separated_by_date[availability_date].append(title)
