326326b37673ad70413be292611a5bde73262628
Israel Silva <israel.bernardes.silva@gmail.com>
2023-02-20 20:04:49 -0300

    feat: download diaries from TST last week and add flake8

--- core/extract_processes_tst.py
+++ core/extract_processes_tst.py
@@ -2,11 +2,12 @@ from datetime import timedelta
 
 import pendulum
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from services.services_browser import ServicesBrowser
 from settings import (COURT_ID, END_DATE_ID, INITIAL_DATE_ID,
-                      SEARCH_BUTTON_XPATH, URL)
+                      SEARCH_BUTTON_XPATH, URL, TABLE_DIARIES_ID)
 
 
 class ExtractProcessesTST:
@@ -21,12 +22,20 @@ class ExtractProcessesTST:
         browser.get(URL)
         return browser
 
-    def search_last_week_tst_diaries(self) -> dict:
+    def search_last_week_tst_diaries(self) -> None:
         start_date, end_date = self.get_start_end_dates_week()
         self.services_browser.fill_input_field(INITIAL_DATE_ID, start_date)
         self.services_browser.fill_input_field(END_DATE_ID, end_date)
         self.services_browser.fill_select_field_by_index(COURT_ID, 1)
         self.services_browser.click_button(SEARCH_BUTTON_XPATH)
+
+    def _download_last_week_tst_diaries(self) -> None:
+        table_with_diaries = self.browser.find_element(By.ID, TABLE_DIARIES_ID)
+        rows = table_with_diaries.find_elements(By.TAG_NAME, "tr")[1:]
+        first_line_table = 2
+
+        for index, row in enumerate(rows, start=first_line_table):
+            self.services_browser.click_button(f'//*[@id="diarioCon"]/fieldset/table/tbody/tr[{index}]/td[3]/button')
 
     @staticmethod
     def get_start_end_dates_week() -> tuple[str, str]:
